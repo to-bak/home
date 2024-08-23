6d2f6cce191faa11ed12a1c0ae2a6cf98acd8439
to-bak <oliverbkp@gmail.com>
2024-08-23 19:30:41 +0200

    update packages

--- home-manager/packages/main.nix
+++ home-manager/packages/main.nix
@@ -1,24 +1,6 @@
 { pkgs, ... }:
 
-with pkgs;
-let
-  default-python = python3.withPackages (python-packages:
-    with python-packages; [
-      pip
-      black
-      flake8
-      setuptools
-      wheel
-      twine
-      flake8
-      virtualenv
-    ]);
-
-  tex = (pkgs.texlive.combine {
-    inherit (pkgs.texlive) scheme-full;
-    });
-
-in {
+{
   home.packages = with pkgs; [
     # MISC
     cachix
@@ -67,13 +49,6 @@ in {
     # spotify
     # spotifyd
     zathura
-
-    # PLANTUML
-    jdk11
-    graphviz
-
-    #Latex Full package
-    tex
 
     #Tilt
     kind
