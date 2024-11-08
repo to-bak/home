fe67b54b296451687cc4ac1a16a4a190183c106b
to-bak <s183569@student.dtu.dk>
2024-11-08 23:21:03 +0100

    Update flake.nix

--- home-manager/flake.nix
+++ home-manager/flake.nix
@@ -34,14 +34,14 @@
             ENV_DIR=$HOME/.flake-env
             if [ -d "$DOT_DIR" ]; then
               echo "PROCEEDING TO BOOTSTRAP" && \
-              cd "$ENV_DIR" && \
-              git add . && \
-              git commit --allow-empty -m "commit flake" && \
               ln -sfn "$DOT_DIR"/home-manager "$HOME"/.config/home-manager && \
               ln -sfn "$DOT_DIR"/nix "$HOME"/.config/nix && \
               ln -sfn "$DOT_DIR"/autorandr "$HOME"/.config/autorandr && \
               ln -sfn "$DOT_DIR"/.emacs.d "$HOME"/.emacs.d && \
               ln -sfn "$DOT_DIR"/.profile "$HOME"/.profile && \
+              cd "$ENV_DIR" && \
+              git add . && \
+              git commit --allow-empty -m "commit flake" && \
               cd "$DOT_DIR"/home-manager && \
               nix flake lock --update-input flake-env && \
               home-manager switch
