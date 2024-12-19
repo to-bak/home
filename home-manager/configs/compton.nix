5ae6db863fc1ff4c229a193a3d3040a090d3d115
To Bak <oliverbkp@gmail.com>
2024-12-20 00:25:53 +0100

    add rounded colors in picom

--- home-manager/configs/compton.nix
+++ home-manager/configs/compton.nix
@@ -8,25 +8,27 @@
     activeOpacity = 0.94;
     inactiveOpacity = 0.86;
     opacityRules = [
-           "100:class_g = 'Chromium-browser'"
-           "100:class_g = 'Google-chrome'"
-           "100:class_g = 'Spotify'"
-           "100:class_g = 'discord'"];
-    menuOpacity = 0.8;
+      "100:class_g = 'Chromium-browser'"
+      "100:class_g = 'Google-chrome'"
+      "100:class_g = 'Spotify'"
+      "100:class_g = 'discord'"];
+      menuOpacity = 0.8;
 
-    vSync = true;
+      vSync = true;
 
-    settings = {
-      frame-opacity = 0.7;
-      blur-method = "dual_kawase";
-      blur-strength = 1;
-      alpha-step = 0.06;
-      detect-client-opacity = true;
-      detect-rounded-corners = true;
-      paint-on-overlay = true;
-      detect-transient = true;
-      mark-wmwin-focused = true;
-      mark-ovredir-focused = true;
-    };
+      settings = {
+        frame-opacity = 0.7;
+        blur-method = "dual_kawase";
+        blur-strength = 1;
+        alpha-step = 0.06;
+        corner-radius = 8.0;
+        round-borders = 1;
+        detect-client-opacity = true;
+        detect-rounded-corners = true;
+        paint-on-overlay = true;
+        detect-transient = true;
+        mark-wmwin-focused = true;
+        mark-ovredir-focused = true;
+      };
   };
 }
