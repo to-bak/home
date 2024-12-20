ba5866e605593ba3351cb3aa3b4cb0bf6b28b443
Oliver-Bak-VCHG38_moto <vchg38@motorolasolutions.com>
2024-12-20 16:19:15 +0100

    add fonts

--- home-manager/configs/stylix.nix
+++ home-manager/configs/stylix.nix
@@ -4,4 +4,19 @@
   stylix.override.base00 = "0c0f0c";
   stylix.base16Scheme = "${pkgs.base16-schemes}/share/themes/shadesmear-dark.yaml";
   stylix.image = ./forest.png;
+
+  stylix.fonts = {
+    monospace = {
+      package = pkgs.nerd-fonts.jetbrains-mono;
+      name = "JetBrainsMono Nerd Font Mono";
+    };
+    sansSerif = {
+      package = pkgs.dejavu_fonts;
+      name = "DejaVu Sans";
+    };
+    serif = {
+      package = pkgs.dejavu_fonts;
+      name = "DejaVu Serfid";
+    };
+  };
 }
