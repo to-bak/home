9c6cb10dc279ddfa1c8e442877579eb880d929e4
Oliver-Bak-VCHG38_moto <vchg38@motorolasolutions.com>
2025-05-29 18:13:37 +0200

    minor updates

--- neovim/init.lua
+++ neovim/init.lua
@@ -1,1 +1,5 @@
+vim.wo.number = true
+vim.opt.clipboard:append("unnamedplus")
+vim.o.ignorecase = true
+
 require "config.lazy"
--- neovim/lua/plugins/telescope.lua
+++ neovim/lua/plugins/telescope.lua
@@ -3,8 +3,28 @@ return {
       dependencies = { 'nvim-lua/plenary.nvim' },
       keys = {
          {"<C-c>pr", function() require("telescope.builtin").live_grep() end, desc = "telescope ripgrep"},
+         {"<C-c>pf", function() require("telescope.builtin").find_files() end, desc = "telescope find_files"},
          {"<C-x>b", function() require("telescope.builtin").buffers() end, desc = "telescope buffers"},
-      }
+      },
+      config = function()
+         require("telescope").setup
+         {
+            defaults = {
+               mappings = {
+                  i = {
+                     ["<C-j>"] = "move_selection_next",
+                     ["<C-k>"] = "move_selection_previous",
+                     ["<C-n>"] = "cycle_history_next",
+                     ["<C-p>"] = "cycle_history_prev",
+                  },
+                  n = {
+                     ["<C-j>"] = "move_selection_next",
+                     ["<C-k>"] = "move_selection_previous",
+                  },
+               },
+            },
+         }
+      end
     },
     {
        'nvim-telescope/telescope-project.nvim',
@@ -19,8 +39,18 @@ return {
        "nvim-telescope/telescope-file-browser.nvim",
        dependencies = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
        keys = {
-          {"<C-x><C-f>", function() require("telescope").extensions.file_browser.file_browser() end , desc = "File browser" }
+          {"<C-c><C-f>", function() require("telescope").extensions.file_browser.file_browser() end , desc = "File browser" }
        },
-       config = function() require("telescope").setup {extensions = {file_browser = {cwd_to_path = true,},},} end
+       config = function()
+          require("telescope").setup
+          {
+             extensions = {
+                file_browser = {
+                   theme = "ivy",
+                   cwd_to_path = true,
+                },
+             },
+          }
+       end
     }
 }
