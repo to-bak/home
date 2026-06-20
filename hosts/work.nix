7d53f7511bc88170d2014ed14155faf85a82b5fd
to-bak <oliverbkp@gmail.com>
2026-06-20 17:49:14 +0200

    update
    g# ------------------------ >8 ------------------------
    diff --git a/hosts/work.nix b/hosts/work.nix
    index 774c477..c55712d 100644
    --- a/hosts/work.nix
    +++ b/hosts/work.nix
    @@ -2,39 +2,80 @@
       config,
       extendedLib,
       pkgs,
    +  nixpkgs,
       nixGL,
       ...
     }:
    
     with extendedLib;
    +let
    +  cfg = config.home;
    +  lib = extendedLib;
    +in
     {
       imports = [
         ../modules/home.nix
    -    # Uncomment modules as needed:
    -    # ../modules/desktop
    -    # ../modules/services
    -    # ../modules/editors
    +    ../modules/desktop
    +    ../modules/services
    +    ../modules/editors
         ../modules/misc
       ];
    
    -  # Identity is read from the environment at build time (requires --impure).
    -  # Run: home-manager switch --flake .#work --impure
       home.username    = builtins.getEnv "USER";
       home.homeDirectory = builtins.getEnv "HOME";
       home.stateVersion = "23.11";
    
    +  home.packages = with pkgs; [
    +    nixgl.nixGLIntel
    +  ];
    +
    +  targets.genericLinux.nixGL = {
    +    packages = nixGL.packages; # you must set this or everything will be a noop
    +    defaultWrapper = "mesa"; # choose from options
    +    installScripts = ["mesa"];
    +    vulkan.enable = false;
    +  };
    +
       nixpkgs.config.allowUnfreePredicate = _: true;
    
    -  # Add work-specific packages here:
    -  # home.packages = with pkgs; [ ];
    +  modules.desktop = {
    +    i3.enable = true;
    +    rofi.enable = true;
    +    polybar.enable = true;
    +    compton.enable = true;
    +  };
    +
    +  modules.editors.neovim.enable = true;
    +  modules.editors.emacs.enable = true;
    +  modules.services.dunst.enable = true;
    
    -  # Add work-specific module configuration here:
       modules.misc = {
         direnv.enable = true;
    +    fzf.enable = true;
    +    kubernetes.enable = true;
    +    ripgrep.enable = true;
    +    lnav.enable = true;
         shell.fish.enable = true;
    +    terminal.alacritty.enable = true;
    +    terminal.tmux = {
    +      enable = true;
    +      sessionizer = {
    +        shallowDirs = [
    +          "$HOME/notes/work"
    +          "$HOME/software/tooling"
    +          "$HOME/.config/home-manager"
    +        ];
    +      };
    +    };
    +  };
    +
    +  home.file.".config/nix/" = {
    +    source = ../configs/nix;
    +    recursive = true;
       };
    
       programs.home-manager.enable = true;
    +  programs.command-not-found.enable = true;
       programs.ssh = {
         enable = true;
         enableDefaultConfig = false;

--- hosts/work.nix
+++ hosts/work.nix
@@ -2,39 +2,80 @@
   config,
   extendedLib,
   pkgs,
+  nixpkgs,
   nixGL,
   ...
 }:
 
 with extendedLib;
+let
+  cfg = config.home;
+  lib = extendedLib;
+in
 {
   imports = [
     ../modules/home.nix
-    # Uncomment modules as needed:
-    # ../modules/desktop
-    # ../modules/services
-    # ../modules/editors
+    ../modules/desktop
+    ../modules/services
+    ../modules/editors
     ../modules/misc
   ];
 
-  # Identity is read from the environment at build time (requires --impure).
-  # Run: home-manager switch --flake .#work --impure
   home.username    = builtins.getEnv "USER";
   home.homeDirectory = builtins.getEnv "HOME";
   home.stateVersion = "23.11";
 
+  home.packages = with pkgs; [
+    nixgl.nixGLIntel
+  ];
+
+  targets.genericLinux.nixGL = {
+    packages = nixGL.packages; # you must set this or everything will be a noop
+    defaultWrapper = "mesa"; # choose from options
+    installScripts = ["mesa"];
+    vulkan.enable = false;
+  };
+
   nixpkgs.config.allowUnfreePredicate = _: true;
 
-  # Add work-specific packages here:
-  # home.packages = with pkgs; [ ];
+  modules.desktop = {
+    i3.enable = true;
+    rofi.enable = true;
+    polybar.enable = true;
+    compton.enable = true;
+  };
 
-  # Add work-specific module configuration here:
+  modules.editors.neovim.enable = true;
+  modules.editors.emacs.enable = true;
+  modules.services.dunst.enable = true;
+
   modules.misc = {
     direnv.enable = true;
+    fzf.enable = true;
+    kubernetes.enable = true;
+    ripgrep.enable = true;
+    lnav.enable = true;
     shell.fish.enable = true;
+    terminal.alacritty.enable = true;
+    terminal.tmux = {
+      enable = true;
+      sessionizer = {
+        shallowDirs = [
+          "$HOME/notes/work"
+          "$HOME/software/tooling"
+          "$HOME/.config/home-manager"
+        ];
+      };
+    };
+  };
+
+  home.file.".config/nix/" = {
+    source = ../configs/nix;
+    recursive = true;
   };
 
   programs.home-manager.enable = true;
+  programs.command-not-found.enable = true;
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
